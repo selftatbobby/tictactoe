5a6cab8966a703b7566fbf461217edba7c19459a
Bobby Tat <messaroundacc1@hotmail.com>
2022-12-28 05:39:52 -0800

    Watching tutorial on how to build tictactoe game

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # tictactoe
 https://www.youtube.com/watch?v=Y-GkMjUZsmM&ab_channel=WebDevSimplified
+-Working on function checkWin() 34:00
--- javascript.js
+++ javascript.js
@@ -1,28 +1,61 @@
-const gameBoard = (() => {
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
 
-};
-const calculator = (() => {
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return {
-      add,
-      sub,
-      mul,
-      div,
-    };
-  })();
+]
+const cellElements = document.querySelectorAll('[data-cell')
+const board = document.getElementById('board')
+let circleTurn
 
-  const gameBoard = (() => {
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return {
-      add,
-      sub,
-      mul,
-      div,
-    };
-  })();
+startGame()
+
+function startGame() {
+    circleTurn = false
+    cellElements.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true})
+    })
+    setBoardHoverClass()
+}
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+
+    }
+    // Check for Draw
+    swapTurns()
+    setBoardHoverClass()
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+        board.classList.add(CIRCLE_CLASS)
+    } else {
+        board.classList.add(X_CLASS)
+    }
+}
+
+function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+        return combination.every()
+    })
+}
